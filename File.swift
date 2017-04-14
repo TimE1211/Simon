07ac59dd5035b58e9f0b6656e1c8c7818232d4d7
TimE1211 <timmy121192@gmail.com>
2017-04-14 15:42:27 -0400

    fixing animation code

--- File.swift
+++ File.swift
@@ -1,11 +1,2 @@
-////
-////  File.swift
-////  Simon
-////
-////  Created by Timothy Hang on 4/13/17.
-////  Copyright © 2017 Timothy Hang. All rights reserved.
-////
-//
-//import Foundation
-//
+
 //creating an animation for each new part of correct array but only animating that one number animation
